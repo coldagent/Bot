489b7d76fb3b4ebc5963f94f33dc2af8942fb441
Zach R <2000zachary.robinson@gmail.com>
2024-03-10 13:34:23 -0500

    Updated counters

--- cogs/counter.py
+++ cogs/counter.py
@@ -10,6 +10,7 @@ class Counter(commands.Cog):
     self.your_mom = []
     self.swears = []
     self.load_counters()
+    profanity.load_censor_words_from_file("./files/bad_words.txt")
     
   def load_counters(self):
     with open("./files/your_mom.csv", mode="r") as file:
@@ -22,7 +23,6 @@ class Counter(commands.Cog):
             self.swears.append(items)
 
   def add_your_mom(self, guild_id):
-    count = 1
     found = False
     for line in self.your_mom:
       if len(line) == 2 and int(line[0]) == guild_id:
@@ -31,16 +31,20 @@ class Counter(commands.Cog):
         line[1] = str(count)
         break
     if not found:
-      self.your_mom.append([str(guild_id), str(count)])
+      self.your_mom.append([str(guild_id), "1"])
     with open("./files/your_mom.csv", mode="w") as file:
       for line in self.your_mom:
         if not len(line) == 2:
           continue
         file.write(line[0] + "," + line[1] + "\n")
-    return count
+
+  def get_your_mom_count(self, guild_id):
+    for line in self.your_mom:
+      if len(line) == 2 and int(line[0]) == guild_id:
+        return int(line[1])
+    return 0
 
   def add_swears(self, guild_id):
-    count = 1
     found = False
     for line in self.swears:
       if len(line) == 2 and int(line[0]) == guild_id:
@@ -49,26 +53,38 @@ class Counter(commands.Cog):
         line[1] = str(count)
         break
     if not found:
-      self.swears.append([str(guild_id), str(count)])
+      self.swears.append([str(guild_id), "1"])
     with open("./files/swears.csv", mode="w") as file:
       for line in self.swears:
         if not len(line) == 2:
           continue
         file.write(line[0] + "," + line[1] + "\n")
-    return count
+
+  def get_swear_count(self, guild_id):
+    for line in self.swears:
+      if len(line) == 2 and int(line[0]) == guild_id:
+        return int(line[1])
+    return 0
 
   @commands.Cog.listener()
   async def on_message(self, message: discord.Message):
     if message.author.id == constants.bot_id:
       return
-    m = re.search("(?:yo|your|ur|tu|ya)\s*(?:mama|mother|mum|mom|madre|mommy)", message.content, re.IGNORECASE)
-    if not m == None:
-      count = self.add_your_mom(message.guild.id)
-      await message.channel.send(f"Your mom counter: {count}")
-    profanity.load_censor_words_from_file("./files/bad_words.txt")
+    mom_words = re.search("(?:yo|your|ur|tu|ya)\s*(?:mama|mother|mum|mom|madre|mommy)", message.content, re.IGNORECASE)
+    if not mom_words == None:
+      self.add_your_mom(message.guild.id)
     if profanity.contains_profanity(message.content):
-      count = self.add_swears(message.guild.id)
-      await message.channel.send(f"Swear counter: {count}")
+      self.add_swears(message.guild.id)
+  
+  @commands.hybrid_command
+  async def counter(self, ctx: commands.Context, arg=""):
+    arg = str.lower(arg)
+    if arg == "mom":
+      await ctx.send(f"Your Mom Count: {self.get_your_mom_count(ctx.guild.id)}")
+    elif arg == "swear":
+      await ctx.send(f"Swear Count: {self.get_swear_count(ctx.guild.id)}")
+    else:
+      await ctx.send("Please specify one of the following counters as a command arg: \n[mom, swear]")
 
 async def setup(bot):
   await bot.add_cog(Counter(bot))
